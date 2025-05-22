66652cf8d37668e994d7f531d09a094ba0e3d1a3
luizaandradeti <lucerchiari@proton.me>
2025-05-22 01:11:55 -0300

    querys

---
+++ pedidos-app/src/main/java/com/luiza/pedidos/registry/AdderAfterReturnAspect.java
@@ -0,0 +1,11 @@
+package com.luiza.pedidos.registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdderAfterReturnAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    public void afterReturn(Object returnValue) throws Throwable {
+        logger.info("value return was {}",  returnValue);
+    }
+}
---
+++ pedidos-app/src/main/java/com/luiza/pedidos/repository/PedidoRepository.java
@@ -0,0 +1,32 @@
+package com.luiza.pedidos.repository;
+
+import com.luiza.pedidos.tabelas.Pedido;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends CrudRepository<Pedido, Long> {
+
+    List<Pedido> findAllByIncluidaIsFalse();
+
+    List<Pedido> findAllByAprovadaIsTrue();
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE pedido SET aprovada = :aprovada, " +
+            "detalhes = :detalhes WHERE id = :id", nativeQuery = true)
+    void atualizarPedido(Long id, boolean aprovada, String detalhes);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Pedido SET incluida = :incluida WHERE id = :id")
+    void atualizarStatusIncluida(Long id, boolean incluida);
+
+
+}
